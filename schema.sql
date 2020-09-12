7435f00e690849bd771747a063e3f312a405aead
Nalee Star <ahn.jinhi@gmail.com>
2020-09-11 20:50:36 -0400

    created role table

--- schema.sql
+++ schema.sql
@@ -5,6 +5,15 @@ CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB; 
 
 CREATE TABLE departments (
-  id INT NOT NULL, 
-  name VARCHAR(30, NULL, 
+  id INT AUTO_INCREMENT NOT NULL, 
+  name VARCHAR(30) NULL, 
+  PRIMARY KEY (id)
 ); 
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL, 
+  title VARCHAR(30) NULL, 
+  salary DECIMAL(10,4) NULL, 
+  deapartment_id INT AUTO_INCREMENT NOT NULL,
+  PRIMARY KEY (id)
+);
