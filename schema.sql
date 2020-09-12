0dd5abad0129e1a408842125d45e0ce34966a20e
Nalee Star <ahn.jinhi@gmail.com>
2020-09-11 20:44:41 -0400

    created schema sql and created dept table

---
+++ schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employee_trackerDB; 
+
+CREATE DATABASE employee_trackerDB; 
+
+USE employee_trackerDB; 
+
+CREATE TABLE departments (
+  id INT NOT NULL, 
+  name VARCHAR(30, NULL, 
+); 
