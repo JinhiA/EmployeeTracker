7dddee3a80c11d4229b8030f6d28a854b4466f81
Nalee Star <ahn.jinhi@gmail.com>
2020-09-17 22:02:13 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -28,7 +28,6 @@ CREATE TABLE employee (
   manager_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE, 
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 SELECT * FROM department;
